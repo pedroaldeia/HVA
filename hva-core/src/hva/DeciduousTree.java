72fe4721117817c24ea87c0c36541c10a6bbde2d
Pedro Aldeia <pedroaldeia@tecnico.ulisboa.pt>
2024-10-05 16:22:33 +0100

    toString and getters for trees

--- hva-core/src/hva/DeciduousTree.java
+++ hva-core/src/hva/DeciduousTree.java
@@ -1,9 +1,33 @@
 package hva;
 
 public class DeciduousTree extends Tree{
-    public String _type = "CADUCA";
+    private String _type = "CADUCA";
+    private String _biologicCycle;
 
-    public DeciduousTree(String id, String name, int age, double cleaningDifficulty){
+    public DeciduousTree(String id, String name, int age, int cleaningDifficulty){
         super(id, name, age, cleaningDifficulty);
+    }
+
+    public String getDifficulty() {
+        return "" + _cleaningDifficulty;
+    }
+
+    public String getAge() {
+        return "" + _age;
+    }
+
+    public String getCycle() {
+        return _biologicCycle;
+    }
+
+    public String getType() {
+        return _type;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ÁRVORE|" + this.getId() + this.getName() + this.getAge() + this.getDifficulty() + this.getType()
+            + this.getCycle();
     }
 }
--- hva-core/src/hva/EvergreenTree.java
+++ hva-core/src/hva/EvergreenTree.java
@@ -1,9 +1,33 @@
 package hva;
 
 public class EvergreenTree extends Tree{
-    public String _type = "PERENE";
+    private String _type = "PERENE";
+    private String _biologicCycle;
 
-    public EvergreenTree(String id, String name, int age, double cleaningDifficulty){
+    public EvergreenTree(String id, String name, int age, int cleaningDifficulty){
         super(id, name, age, cleaningDifficulty);
+    }
+
+
+    public String getDifficulty() {
+        return "" + _cleaningDifficulty;
+    }
+
+    public String getAge() {
+        return "" + _age;
+    }
+
+    public String getCycle() {
+        return _biologicCycle;
+    }
+
+    public String getType() {
+        return _type;
+    }
+
+    @Override
+    public String toString() {
+        return "ÁRVORE|" + this.getId() + this.getName() + this.getAge() + this.getDifficulty() + this.getType()
+            + this.getCycle();
     }
 }
